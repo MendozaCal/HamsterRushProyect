84efdeb31a263f5694753bdba29536165ef6f3c6
Pablo Mendoza <asdson5555@gmail.com>
2024-04-25 14:08:44 -0500

    Nitro System

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Move
     public float maxSpeed = 15;
     float speed;
     public float accelerationTime = 10;
     public float rotationSpeed = 45;
     private float currentSpeed = 0;
     private float acceleration;
+    //Nitro
+    public float maxNitro = 100;
+    public float TimerNitro = 0;
+    public float maxTimeNitro = 5;
+    public float nitroPower = 10;
     
     //impulso rampa
     public float TimerImpulso = 0;
@@ -19,24 +25,18 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        maxSpeed = Mathf.Min(maxSpeed, 500);
+        maxNitro = Mathf.Min(maxNitro, 100);
+        maxNitro = Mathf.Max(maxNitro, 0);
         acceleration = maxSpeed / accelerationTime;
         speed = maxSpeed;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         MoveHamster();
-        if(impulsoVerification == true)
-        {
-            TimerImpulso += Time.deltaTime;
-            if (TimerImpulso >= MaxTimeImpulso)
-            {
-                maxSpeed = speed;
-                impulsoVerification = false;
-                TimerImpulso = 0;
-            }
-        }
-        
+        BustRampa();
+        Nitro();
     }
     public void MoveHamster()
     {
@@ -50,6 +50,31 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(movement, Space.Self); //Encargado de movimiento //Space.Self = para q se quede mirando a donde giraste
         transform.Rotate(rotation.normalized);
+    }
+    public void Nitro()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && maxNitro >= 0)
+        {
+            maxNitro -= Time.deltaTime * 10;
+            maxSpeed *= nitroPower;
+        }
+        else
+        {
+            maxSpeed = speed;
+        }
+    }
+    public void BustRampa()
+    {
+        if (impulsoVerification == true)
+        {
+            TimerImpulso += Time.deltaTime;
+            if (TimerImpulso >= MaxTimeImpulso)
+            {
+                maxSpeed = speed;
+                impulsoVerification = false;
+                TimerImpulso = 0;
+            }
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
