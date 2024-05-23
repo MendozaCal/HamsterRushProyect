6a030b7ab3dc144c5ebdef76fa61b6464c3f4cc3
Pablo Mendoza <147556543+MendozaCal@users.noreply.github.com>
2024-05-23 03:11:18 -0500

    vayado y correction Move

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -46,9 +46,15 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput) * maxSpeed * Time.deltaTime;
+        // Calcular el vector de movimiento en función de la entrada del usuario
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput) * maxSpeed;
+        movement.y = rb.velocity.y;
+
+        // Aplicar el movimiento al Rigidbody
+        rb.velocity = transform.TransformDirection(movement);
+
+        // Calcular y aplicar la rotación
         Quaternion rotation = Quaternion.Euler(0, horizontalInput * rotationSpeed * Time.deltaTime, 0);
-        rb.MovePosition(transform.position + transform.TransformDirection(movement));
         rb.MoveRotation(rb.rotation * rotation);
     }
     public void Nitro()
